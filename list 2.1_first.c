df48917e7e7e359f48be694b66f9215f5f81759d
simple-ginger-bird <blepch.rkb0v@slmail.me>
2022-05-12 14:37:23 +0900

    Add list 2.1

---
+++ list 2.1_first.c
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main(void)	/* 간단한 프로그램 */
+{
+	int num;
+	num = 1;
+	printf("나는 단순한 ");
+	printf("컴퓨터이다.\n");
+	printf("나는 최고를 원하기 때문에 %d이라는 숫자를 좋아한다.\n", num);
+
+	return 0;
+}
