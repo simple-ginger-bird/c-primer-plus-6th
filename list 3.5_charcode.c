b5b3ee2536fe022896b48458a1f6213cfbd11251
simple-ginger-bird <blepch.rkb0v@slmail.me>
2022-05-12 16:01:59 +0900

    Add list 3.5

---
+++ list 3.5_charcode.c
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+{
+	char ch;
+
+	printf("키보드를 영문 모드로 놓고, 문자 하나를 입력하시오.\n");
+	scanf("%c", &ch);
+	printf("문자 %c에 대응하는 코드값은 %d이다.\n", ch, ch);
+
+	return 0;
+}
