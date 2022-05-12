4ba1a3fab549625bfab9e2916b672c8dc3ff66a4
simple-ginger-bird <blepch.rkb0v@slmail.me>
2022-05-12 15:15:32 +0900

    Add list 2.4

---
+++ list 2.4_nogood.c
@@ -0,0 +1,17 @@
+/* nogood.c -- 몇 개의 에러가 있는 프로그램*/
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main(void)
+(
+	int n, int n2, int n3;
+
+	/* 이 프로그램은 몇 개의 에러를 가지고 있다
+	   n = 5;
+	   n2 = n * n;
+	   n3 = n2 * n2
+	   printf("n = %d, n의 제곱 = %d, n의 세제곱 = %d\n", n, n2, n3)
+	   
+	   return 0;
+	   */
+)
