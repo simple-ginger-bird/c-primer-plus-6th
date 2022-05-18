a60b0264e3e93b356c91a12917e00b1de97af714
simple-ginger-bird <blepch.rkb0v@slmail.me>
2022-05-18 19:08:37 +0900

    Add list 4.2

---
+++ Chapter_4/list 4.2_praise1.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define PRAISE "You are an extraordinary being."
+
+int main(void)
+{
+	char name[40];
+
+	printf("실례지만 성함이 어떻게 되시는지?\n");
+	scanf("%s", name);
+	printf("반갑습니다, %s씨. %s\n", name, PRAISE);
+	
+	return 0;
+}
