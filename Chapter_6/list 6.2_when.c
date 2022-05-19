6f79888d8214dcf7facf7e4500eca7f8173caa70
simple-ginger-bird <blepch.rkb0v@slmail.me>
2022-05-19 15:06:36 +0900

    Add list 6.2

---
+++ Chapter_6/list 6.2_when.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n = 5;
+
+	while (n < 7)
+	{
+		printf("n = %d\n", n);
+		n++;
+		printf("捞力 n = %d\n", n);
+	}
+	printf("风橇啊 场车促.\n");
+
+	return 0;
+}
