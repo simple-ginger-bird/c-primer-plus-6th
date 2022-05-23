f6b508fae7b78ca10ba85ba165b2c79354bdfeab
simple-ginger-bird <blepch.rkb0v@slmail.me>
2022-05-23 16:11:38 +0900

    Add list 10.4

---
+++ Chapter_10/list 10.4_day_mon2.c
@@ -0,0 +1,14 @@
+// day_mon2.c -- 컴파일러가 원소 개수를 카운트한다
+#include <stdio.h>
+
+int main(void)
+{
+	const int days[] = { 31,28,31,30,31,30,31,31,30,31 };	// 값을 10개만 입력했다
+	int index;
+
+	for (index = 0; index < sizeof days / sizeof days[0]; index++)
+		printf("%2d월: 날짜 수 %2d\n", index + 1,
+			days[index]);
+
+	return 0;
+}
