e7a68d18f701fd8060db70017257a01e05b6527b
simple-ginger-bird <blepch.rkb0v@slmail.me>
2022-05-25 16:35:34 +0900

    Add list 10.18 - not support VLA

---
+++ Chapter_10/list 10.18_vararr2d.c
@@ -0,0 +1,8 @@
+// vararr2d.c -- VLA를 사용하는 함수
+// Visual Studio에선 컴파일 불가(코드 E0411 : 매개 변수를 사용할 수 없습니다.
+
+#include <stdio.h>
+#define ROWS 3
+#define COLS 4
+
+// int sum2d(int rows, int cols, int ar[rows][cols]);
